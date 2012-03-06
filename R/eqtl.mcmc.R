cd512d725d108dd6e56fad3674c1eeefed7b47a5
Gregory Imholte <gimholte@gmail.com>
2012-03-06 11:18:22 -0800

    updated call to iBMQ C code

--- R/eqtl.mcmc.R
+++ R/eqtl.mcmc.R
@@ -36,23 +36,23 @@ stop("there is a problem with indivs names")
 }
 
 pheno <- unlist(expr)
-
 #call "c_qtl_mcmc" of C code
 start.time <- Sys.time()
 outProb = double(length = n.snp*n.pheno)
 
 c.function="c_qtl_main_parallel_sparse"
 #res <- .C("c_qtl_mcmc_wjg_new",as.double(pheno),as.integer(n.indiv),as.integer(n.pheno),as.double(snp),as.integer(n.snp),as.integer(n.iter),as.integer(burn.in),as.integer(n.sweep))
+eps = 10*(.Machine$double.eps)
+nmax = 10
 res <- .C(c.function,as.double(pheno),as.integer(n.indiv),
-  as.integer(n.pheno),as.double(snp),as.integer(n.snp),
-  as.integer(n.iter),as.integer(burn.in),as.integer(n.sweep),
-  as.double(outProb), as.integer(nproc))
+		as.integer(n.pheno),as.double(snp),as.integer(n.snp),
+		as.integer(n.iter),as.integer(burn.in),as.integer(n.sweep),
+		as.double(outProb), as.integer(nproc), as.integer(nmax), as.double(eps))
 end.time <- Sys.time()
 cat("running MCMC takes ")
-    cat(as.character(round(difftime(end.time,start.time,units="min"),digits=2)))
-    cat(" minutes.\n")
-
-prob=read.table("prob_include_file.txt")
-return(prob)
+cat(as.character(round(difftime(end.time,start.time,units="min"),digits=2)))
+cat(" minutes.\n")
+dim(res[[9]]) = c(n.snp, n.pheno)
+return(res[[9]])
 }
 
