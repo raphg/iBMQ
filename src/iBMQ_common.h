cce03461bdfaf01aecceb4c52e0b40174041275f
Gregory Imholte <gimholte@gmail.com>
2012-04-16 06:50:16 -0700

    update function headers.

--- src/iBMQ_common.h
+++ src/iBMQ_common.h
@@ -45,7 +45,44 @@ void store_prob_include(int *n_iter, int *n_snps, int *n_genes, int** ProbSum, d
 
 void update_prob_include(int* n_snps, int* n_genes, int** Gamma, int** ProbSum);
 
-int update_pos_j(double* P, double* A, double* B, double* C, double** W, int** Gamma,
+inline double log1m_from_logit(double x);
+
+inline double log_from_logit(double x);
+
+inline double expit(double x)
+{
+	double expit;
+	expit = 1.0/(1.0 + exp(-x));
+	return(expit);
+}
+
+inline double log_from_logit(double x)
+{
+	if(x > 0.0)
+	{
+		return(-log1p(exp(-x)));
+	}
+	else
+	{
+		return(x - log1p(exp(x)));
+	}
+}
+
+inline double log1m_from_logit(double x)
+{
+	if(x > 0.0)
+	{
+		return(-x - log1p(exp(-x)));
+	}
+	else
+	{
+		return(-log1p(exp(x)));
+	}
+}
+
+
+int update_pos_j(double* P, double* A, double* B, double* C, double** W_Logit,
+		int** W_Ind, int** Gamma,
 		int j, double* a_0, double* b_0, double* lambda_a, double* lambda_b,
 		int* n_snps, int* n_genes, int* n_indivs, RngStream rng, int nmax,
 		double *xA, double *xB, ARS_workspace *workspace, double eps);
@@ -54,12 +91,13 @@ void set_prior(double* lambda_a, double* lambda_b, double* a_0, double* b_0, dou
 		double* expr_means, double* expr_vars, double* alpha2_beta,
 		gsl_matrix* X, gsl_matrix* Y, RngStream rng);
 
-void initialize_parms(m_el **Beta, int** Gamma, double** W, int** ProbSum, double **xA, double **xB,
+void initialize_parms(m_el **Beta, int** Gamma, double** W_Logit, int** W_Ind, int** ProbSum, double **xA, double **xB,
 		double* A, double* B, double* C, double* P, double* Mu, double* Sig2,
 		double* expr_means, double* expr_vars, double* alpha2_beta,
 		double* lambda_a, double* a_0, double* lambda_b, double* b_0, double* tau_0,
 		int* n_snps, int* n_genes, int* n_indivs, int* nmax, RngStream rng);
 
+inline double expit(double x);
 #endif /* IBMQ_COMMON_H_ */
 
 
