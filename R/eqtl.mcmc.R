d3f0a66bb8d1bbb09758705df94ee79612b95959
Gregory Imholte <gimholte@gmail.com>
2012-04-18 00:41:17 -0700

    add argument to allow user to select whether to write chain to file add argument to allow user to let "C" variables be constant

--- R/eqtl.mcmc.R
+++ R/eqtl.mcmc.R
@@ -1,5 +1,5 @@
 eqtl.mcmc <-
-function(snp,expr,n.iter,burn.in,n.sweep,nproc)
+function(snp,expr,n.iter,burn.in,n.sweep,nproc, constC = TRUE, write.output = TRUE)
 {
 n.pheno <- length(expr)
 n.snp <- length(snp)
@@ -43,11 +43,12 @@ outProb = double(length = n.snp*n.pheno)
 c.function="c_qtl_main_parallel_sparse"
 #res <- .C("c_qtl_mcmc_wjg_new",as.double(pheno),as.integer(n.indiv),as.integer(n.pheno),as.double(snp),as.integer(n.snp),as.integer(n.iter),as.integer(burn.in),as.integer(n.sweep))
 eps = 10*(.Machine$double.eps)
-nmax = 10
+nmax = 100
 res <- .C(c.function,as.double(pheno),as.integer(n.indiv),
 		as.integer(n.pheno),as.double(snp),as.integer(n.snp),
 		as.integer(n.iter),as.integer(burn.in),as.integer(n.sweep),
-		as.double(outProb), as.integer(nproc), as.integer(nmax), as.double(eps))
+		as.double(outProb), as.integer(nproc), as.integer(nmax), as.double(eps),
+		as.integer(write.output), as.integer(constC))
 end.time <- Sys.time()
 cat("running MCMC takes ")
 cat(as.character(round(difftime(end.time,start.time,units="min"),digits=2)))
